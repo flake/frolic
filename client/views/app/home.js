cbb176dc19cf2819fab2e98cd9eaaf459b07221c
Rajasekhar <rajasekahr@ynpole.com>
2016-02-17 01:31:05 +0530

    AppBar render fix

--- client/views/app/home.js
+++ client/views/app/home.js
@@ -6,25 +6,28 @@ Template.home.helpers({
   FroScreen: function(){
     return FroScreen;
   },
-  froFS: function(){
-    // console.log("Frolic Id: " + this.froId);
-    var froFS = {
-      fro: null,
-      thumb: null
-    };
-    var fro = Fros.findOne(Session.get("froPlay"));
-
-    if(fro){
-      var froFs = FroFS.findOne(fro.fsId);
-      froFS.fro = froFs.url();
-      froFS.thumb = ThumbFS.findOne(fro.thumb_fs).url();
-    }
-
-    return froFS;
+  froDoc: function(){
+    return Fros.findOne(Session.get("froPlay"));
   },
-  SpaceBar: function(){
-    return SpaceBar;
-  },
+  // froFS: function(){
+  //   // console.log("Frolic Id: " + this.froId);
+  //   var froFS = {
+  //     fro: null,
+  //     thumb: null
+  //   };
+  //   var fro = Fros.findOne(Session.get("froPlay"));
+  //
+  //   if(fro){
+  //     var froFs = FroFS.findOne(fro.fsId);
+  //     froFS.fro = froFs.url();
+  //     froFS.thumb = ThumbFS.findOne(fro.thumb_fs).url();
+  //   }
+  //
+  //   return froFS;
+  // },
+  // SpaceBar: function(){
+  //   return SpaceBar;
+  // },
   isPlaying: function(){
     return Session.get("isPlaying");
   }
